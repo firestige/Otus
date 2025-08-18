eee6112a513b72ee8e64c29a7a752b8c6a935c9c
firestige <zzt.nbl@hotmail.com>
2025-08-18 14:33:02 +0000

    调整文档位置

--- internal/otus/capture/sniffer/sniffer.go
+++ internal/otus/capture/sniffer/sniffer.go
@@ -1,7 +1,22 @@
 package sniffer
 
+import (
+	"context"
+
+	"firestige.xyz/otus/internal/otus/capture/codec"
+)
+
 type Sniffer struct {
 	NetInterface any
 	Snaplen      int
 	LocalIP      string
+}
+
+func NewSniffer(decoder *codec.Decoder, options *Options, ctx context.Context) *Sniffer {
+}
+
+func (s *Sniffer) Start(ctx context.Context) error {
+}
+
+func (s *Sniffer) Stop() {
 }
